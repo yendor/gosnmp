0639d24bf5256fe0cc0b37f1881fe7af689c7660
Rodney Amato <rodnet@gmail.com>
2013-11-19 09:34:45 +1100

    Revert the experiment with GetBulkRequest for now

--- marshal.go
+++ marshal.go
@@ -161,7 +161,7 @@ func (packet *SnmpPacket) marshalPDU(oids []string, requestid uint32) ([]byte, e
 
 	// build up resulting pdu - GetRequest, length, then the tail (buf)
 	pdu := new(bytes.Buffer)
-	pdu.WriteByte(byte(GetBulkRequest))
+	pdu.WriteByte(byte(GetRequest))
 
 	buf_length_bytes, err2 := marshalLength(buf.Len())
 	if err2 != nil {
